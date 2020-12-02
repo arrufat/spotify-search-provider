78e5f84ee09f38e204cbec126a5ffb65ab8efb8c
Adrià Arrufat <adria.arrufat@outlook.com>
2020-12-02 22:27:39 +0900

    add play and queue commands, only works prepended by $

--- spotify-search-provider.py
+++ spotify-search-provider.py
@@ -17,8 +17,9 @@ search_bus_name = "org.gnome.Shell.SearchProvider2"
 sbn = dict(dbus_interface=search_bus_name)
 
 CLIENT_ID = "9226139be2064e9890c50b8021bbbfbf"
-ALBUMART_DIR = os.path.expanduser("~/.cache/gssp-spotify/albumart")
-CACHE_PATH = os.path.expanduser("~/.cache/gssp-spotify/.cache")
+CACHE_DIR = os.path.expanduser("~/.cache/spotify-search-provider/")
+ALBUMART_DIR = os.path.join(CACHE_DIR, "albumart")
+CACHE_FILE = os.path.join(CACHE_DIR, ".cache")
 REDIRECT_URI = "http://localhost:8888/callback"
 ACCESS_SCOPE = "user-modify-playback-state user-read-playback-state"
 
@@ -55,29 +56,53 @@ class SpotifySearchProvider(dbus.service.Object):
     def __init__(self):
         self.results = []
         self.session_bus = dbus.SessionBus()
+        self.command = None
         bus_name = dbus.service.BusName(self.bus_name, bus=self.session_bus)
         dbus.service.Object.__init__(self, bus_name, self._object_path)
-        os.makedirs(os.path.dirname(CACHE_PATH), exist_ok=True)
         os.makedirs(ALBUMART_DIR, exist_ok=True)
 
         self.auth_manager = SpotifyPKCE(
             client_id=CLIENT_ID,
-            cache_path=CACHE_PATH,
+            cache_path=CACHE_FILE,
             redirect_uri=REDIRECT_URI,
             scope=ACCESS_SCOPE,
         )
 
         self.spotify = Spotify(auth_manager=self.auth_manager)
-        self.spotify.current_user()
 
     @dbus.service.method(in_signature="sasu", **sbn)
     def ActivateResult(self, id, terms, timestamp):
-        self.spotify.start_playback(uris=[id])
+        debug(terms)
+        cmd = terms[0][1:]
+        if cmd == "":  # queue by default
+            cmd = "queue"
+        debug(cmd)
+        msg = None
+        if cmd == "play":
+            self.spotify.start_playback(uris=[id])
+            msg = "Now playing"
+        elif cmd == "queue" or cmd == "q":
+            self.spotify.add_to_queue(uri=id)
+            msg = "Added to queue"
+        else:
+            self.notify("Unknown Command", body=cmd)
+            return
+
+        self.notify(
+            msg,
+            body=self.results[id]["artist"] + " - " + self.results[id]["title"],
+            icon=self.results[id]["icon"],
+        )
 
     @dbus.service.method(in_signature="as", out_signature="as", **sbn)
     def GetInitialResultSet(self, terms):
+        # only parse queries that start with $ and contain 2 or more terms
+        if not terms[0].startswith("$"):
+            return []
+        if len(terms) < 2:
+            return []
         qs = ""
-        for q in terms:
+        for q in terms[1:]:
             qs += q + " "
         qs.strip()
         if len(qs) < 2:
@@ -117,7 +142,7 @@ class SpotifySearchProvider(dbus.service.Object):
                     with open(albumart_path, "wb") as f:
                         with urlopen(self.results[uri]["albumart_url"]) as r:
                             f.write(r.read())
-
+                self.results[uri]["icon"] = albumart_path
                 entry["gicon"] = albumart_path
                 out.append(entry)
         return out
@@ -125,6 +150,24 @@ class SpotifySearchProvider(dbus.service.Object):
     @dbus.service.method(in_signature="asu", terms="as", timestamp="u", **sbn)
     def LaunchSearch(self, terms, timestamp):
         pass
+
+    def notify(self, message, body="", icon="spotify-client", error=False):
+        try:
+            self.session_bus.get_object(
+                "org.freedesktop.Notifications", "/org/freedesktop/Notifications"
+            ).Notify(
+                "Spotify",
+                0,
+                icon,
+                message,
+                body,
+                "",
+                {"transient": False if error else True},
+                0 if error else 3000,
+                dbus_interface="org.freedesktop.Notifications",
+            )
+        except dbus.DBusException as err:
+            print(f"Error {err} while trying to display {message}.")
 
 
 if __name__ == "__main__":
